b8c286007a11c0888c607344d76cdae1f3f94f79
UVa student <student@student-VirtualBox.(none)>
2014-04-05 23:44:43 -0400

    cd works

--- kernel/fs.rs
+++ kernel/fs.rs
@@ -58,71 +58,13 @@ pub unsafe fn listDir(givenDir: directory) {
         putcstr(fi.name);
         drawcstr(fi.name, true, false);
     }
-
-
-
-    // for dir in *(givenDir.dchildren) {
-    //     let name = dir.name;
-    //     ret.push(name);
-    // }
-    // for fi in *(givenDir.fchildren) {
-    //     let name = fi.name;
-    //     ret.push(name);
-    // }
 }
 
-/*
-pub fn open(node: *tree_node, file: cstr) -> (*tree_node, bool, bool)
-{
-    if dir.isLeaf() || file == ""
-    {
-	return (node, dir.isLeaf(), file == "");
+pub unsafe fn cd(givenDir: directory, goal: cstr) -> (bool,directory) {
+    for dir in iter((*givenDir.dchildren).as_slice()) {
+        if dir.name.eq(&goal) {
+            return (true,*dir)
+        }
     }
-    let mut children: uint = (*node).child_count;
-    let mut i: uint = 0;
-    let mut split = file.before('/');
-    while i < children
-    {
-	if (*node).children[i].name == k
-	{
-	    return open((*node).children[i], file.remainder('/'));
-	} else
-	{
-	    i += 1;
-	}
-    }
-    return cstr::new();
+    return (false,givenDir)
 }
-
-pub fn append(node: *tree_node, file: cstr, content: cstr) -> bool
-{
-    let (mut f, _, _) = open(node, file);
-    if f == cstr::new()
-    {
-	return false;
-    }
-    let mut x = 0;
-    let mut f_contents = (*f).contents;
-    while x < content.len()
-    {
-	let b = f_contents.push_char(content.char_at(x));
-	if !b
-	{
-	    return false;
-	}
-    }
-    let (*f).contents = f_contents;
-    return true;
-}
-
-pub fn new(node: *tree_node, dir: cstr, name: cstr) -> bool
-{
-    let (mut n, _, _) = open(node, file);
-    if !n.isLeaf()
-    {
-	n.insert(name);
-    }
-    return false;
-
-}
-*/
--- kernel/sgash.rs
+++ kernel/sgash.rs
@@ -278,57 +278,35 @@ unsafe fn parse() {
 			putstr(&"\nTEST cat");
 			drawstr(&"\nTEST cat");
 		} else if(y.streq(&"cd")) {
-			/*match buffer.getarg(' ', 1) {
+			match buffer.getarg(' ', 1) {
 				Some(mut word) => {
-					if(word.eq(&cstr::from_str(&".."))) {
-						let padre = cwd.parent as *dnode;
-						cwd = *padre;
-						putstr(&"Current Directory is ");
-						putcstr(cwd.name);
-						drawstr(&"Current Directory is ");
-						drawcstr(cwd.name,true,false);
-					}
-
-
-					let mut i = 0;
-					if cwd.len() == 0 {
-						putstr(&"ZERO");
-					} else if cwd.len() < 0 {
-						putstr(&"Below");
-					} else if cwd.len() > 0 {
-						putstr(&"Above");
-					}
-					let mut found = false;
-					while i < cwd.len() {
-						let dir = cwd.get_dir(i);
-						if dir != '\0' as u32 {
-							let ptr = dir as *dnode;
-							let t = *ptr;
-							if (word.eq(&t.name)) {
-								cwd = *ptr;
-								found = true;
-							}
-						} else {
-							putstr(&"Got something null");
-						}
-						i = i + 1;
-					}
-					if(found) {
-						putstr(&"Current Directory is ");
-						putcstr(cwd.name);
-						drawstr(&"Current Directory is ");
-						drawcstr(cwd.name,true,false);
-					}
-					else {
-
-					}
+			         let check = fs::cd(cwd,word);
+                     match check {
+                        (x,y) => {
+                            if x {
+                                cwd = y;
+                                putstr(&"Current folder");
+                                putcstr(cwd.name);
+                                drawstr(&"Current folder");
+                                drawcstr(cwd.name,true,false);
+                            }
+                            else {
+                                putstr(&"Directory does not exist\n");
+                                drawstr(&"Directory does not exist\n");
+                            } 
+                        }
+                        /*( _, _) => {
+                            putstr(&"Something horrible has occurred\n");
+                            drawstr(&"Something horrible has occurred\n");
+                        }*/
+                     }
 				}
 				None => {
 					putstr(&"Bad Directory Name\n");
 					drawstr(&"Bad Directory Name\n");
 				}
 			}
-            */
+            
 
 
 		} else if(y.streq(&"rm")) {
@@ -465,7 +443,7 @@ impl cstr {
 	}
 
 	#[allow(dead_code)]
-	unsafe fn eq(&self, other: &cstr) -> bool {
+	pub unsafe fn eq(&self, other: &cstr) -> bool {
 		if (self.len() != other.len()) { return false; }
 		else {
 			let mut x = 0;
